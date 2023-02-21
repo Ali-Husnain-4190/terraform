99c636a27f6af34a235ad7ebea2942472ebab4c8
alihusnain4190 <alihusnain4190@gmail.com>
2023-02-21 13:13:18 +0000

    vpc

--- proj/vpc/main.tf
+++ proj/vpc/main.tf
@@ -5,7 +5,7 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-resource "aws_subnet" "my_subnet" {
+resource "aws_subnet" "public_subnet" {
   cidr_block              = var.public_subnet_cidr
   vpc_id                  = aws_vpc.my_vpc.id
   map_public_ip_on_launch = true
@@ -13,18 +13,26 @@ resource "aws_subnet" "my_subnet" {
     "Name" = var.public_subnet
   }
 }
-data "aws_availability_zones" "available" {}
 resource "aws_subnet" "private_subnet_1" {
-  cidr_block              = element(var.private_subnet_cidr, count.index)
+  cidr_block              = var.private_subnet_1_cidr
   vpc_id                  = aws_vpc.my_vpc.id
   map_public_ip_on_launch = false
-  count                   = 1
-  #availability_zone       = element(var.availability_zones, count.index)
-  availability_zone = data.aws_availability_zones.available.names[count.index]
+  availability_zone       = "us-east-1a"
+
   tags = {
     "Name" = var.private_subnet_1
   }
 }
+# resource "aws_subnet" "private_subnet_2" {
+#   cidr_block              = var.private_subnet_2_cidr
+#   vpc_id                  = aws_vpc.my_vpc.id
+#   map_public_ip_on_launch = false
+
+#   availability_zone = "us-east-1b"
+#   tags = {
+#     "Name" = var.private_subnet_2
+#   }
+# }
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.my_vpc.id
 
@@ -32,18 +40,49 @@ resource "aws_internet_gateway" "gw" {
     Name = "my_igw"
   }
 }
-
-resource "aws_route_table" "public_rtb" {
+resource "aws_route_table" "my_public_rtb" {
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "public_rtb"
+    Name = "public route"
   }
 }
-resource "aws_route_table" "private_rtb" {
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.my_public_rtb.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+resource "aws_route_table_association" "public_route_association" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.my_public_rtb.id
+}
+resource "aws_route_table" "my_private" {
   vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "public route"
+  }
+}
+# resource "aws_route" "default_route" {
+#   route_table_id         = aws_route_table.my_public_rtb.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_internet_gateway.gw.id
+# }
+resource "aws_route_table_association" "private_route_association" {
+  subnet_id      = aws_subnet.private_subnet_1.id
+  route_table_id = aws_route_table.my_private.id
+}
+resource "aws_eip" "nat_gateway" {
+  vpc = true
+}
+resource "aws_nat_gateway" "NGW" {
+  allocation_id = aws_eip.nat_gateway.id
+  subnet_id     = aws_subnet.public_subnet.id
 
   tags = {
-    Name = "private_rtb"
+    Name = "public subnet NAT gateway"
   }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+  depends_on = [aws_internet_gateway.gw]
 }
--- proj/vpc/variable.tf
+++ proj/vpc/variable.tf
@@ -27,21 +27,30 @@ variable "private_subnet_1" {
 
 }
 
-# variable "private_subnet_1_cidr" {
-#   type    = string
-#   default = "10.0.1.0/24"
+variable "private_subnet_1_cidr" {
+  type    = string
+  default = "10.0.1.0/24"
 
-# }
+}
 variable "private_subnet_2" {
   type    = string
   default = "private_subnet_2"
 
 }
-variable "private_subnet_cidr" {
-  type    = list(any)
-  default = ["10.0.2.0/24", "10.0.1.0/24"]
+variable "private_subnet_2_cidr" {
+  type    = string
+  default = "10.0.2.0/24"
 }
-variable "availability_zones" {
-  type    = list(string)
-  default = ["us-east-1a", "us-east-1b"]
+# variable "availability_zones" {
+#   type    = list(string)
+#   default = ["us-east-1a", "us-east-1b"]
+# }
+
+variable "region_number" {
+  # Arbitrary mapping of region name to number to use in
+  # a VPC's CIDR prefix.
+  default = {
+    us-east-1a = 1
+    us-east-1b = 2
+  }
 }
